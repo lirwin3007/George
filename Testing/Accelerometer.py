404b8375b92de82d52512215c9337784400b9c53
Louis Irwin <louis.irwin.3007@gmail.com>
2018-04-15 14:58:22 +0000

    Added testing folder

--- Testing/Accelerometer.py
+++ Testing/Accelerometer.py
@@ -1,4 +1,5 @@
 import Adafruit_LSM303
+import os, time
 
 lsm303 = Adafruit_LSM303.LSM303()
 
@@ -8,6 +9,7 @@ while True:
     # Grab the X, Y, Z components from the reading and print them out.
     accel_x, accel_y, accel_z = accel
     mag_x, mag_z, mag_y = mag
+    os.system('clear')
     print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(
           accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
-    # Wait half a second and repeat.
+    time.sleep(0.1)
